1df58c4a665d58de0e9592a8c3033806191d6b8c
sufi0900 <108760976+sufi0900@users.noreply.github.com>
2024-03-03 20:21:39 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          500: '#e60045',
+        },
+        secondary: {
+          500: '#22262a',
+          600: '#16191d',
+
+        }
+      },
+    },
+  },
+  plugins: [],
+  
+}
